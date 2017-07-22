14f11d6c09aefe891e04867c0fb20bc9afce1720
jdetras <jdetras@gmail.com>
2017-07-22 21:16:29 +0800

    added annotate_text.py from NCBO

--- ontology-mapper.py
+++ ontology-mapper.py
@@ -5,36 +5,29 @@
 # Date: 07-21-2017
 
 #import modules
-import sys
-import getopt
 import pandas as pd
+import urllib2
+from annotate_text import get_json
+from annotate_text import print_annotations
 
-#import trait dictionary as a dataframe
-td_file = 'TD_CO_320_Rice_EN_v5_v160323.csv'
-td = pd.read_csv(td_file, sep=',', header=0)
-td_df = pd.DataFrame(td)
-#print td_df
-
-#copy trait ID and trait name into a new dataframe
-td_trait_header = 'Trait name'
-td_trait_id = 'Trait ID'
-trait_name = td_df[[td_trait_id, td_trait_header]].copy()
-
-#drop duplicate entries for the traits
-trait_name.drop_duplicates(keep=False, inplace=True)
-#print trait_name
-
-#import query traits for annotation
-query_file = 'table.csv'
+#import query traits as pandas dataframe
+query_file = 'table2.csv' #file
 query = pd.read_csv(query_file, sep=',', header=0)
 query_df = pd.DataFrame(query)
-#print query_df
 
-#assign variable for query trait name column header
+#declare variable for query trait name column header
 tn_header = 'trait_name'
 
-#search for exact match for trait_name in query_df
-## currently working for exact match
+#web service URL
+REST_URL = "http://data.agroportal.lirmm.fr"
 
-for row in trait_name.itertuples():
-    print query_df.loc[query_df[tn_header].isin(trait_name[td_trait_header])]
+#row count for query trait
+row = 1
+
+#do the API call for each trait
+for text_to_annotate in query_df[tn_header]:
+    annotations = get_json(REST_URL + "/annotator?&ontologies=CO_320&text=" + urllib2.quote(text_to_annotate)) 
+    #print query trait, row and annotation from web service call
+    print 'Query trait: ' + str(row) + ' : ' + text_to_annotate
+    print_annotations(annotations)
+    row = row + 1
